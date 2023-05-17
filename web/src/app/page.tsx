fbe1ea2d00898d9f61a62cfdbc4cba0199dd2f3c
luix gabriel <luisgasbriel@gmail.com>
2023-05-17 11:56:08 -0300

    advancing

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,21 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Hello World</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+        {/* 1/2 = 50% */}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+      </div>
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, começe a{' '}
+            {/* OS COLCHETES ME AJUDAM A COLOCAR A INFORMAÇÃO QUE EU QUISER */}
+            <a href="#" className="underline hover:text-gray-50">
+              criar agora!
+            </a>{' '}
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
