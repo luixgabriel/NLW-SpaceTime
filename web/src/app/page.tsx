a8cfa3f1824a893891eac31e50cd2ac160eb474e
luix gabriel <luisgasbriel@gmail.com>
2023-05-18 11:12:43 -0300

    advancing

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,4 +1,6 @@
 import { User } from 'lucide-react'
+import Image from 'next/image'
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
 
 export default function Home() {
   return (
@@ -8,6 +10,7 @@ export default function Home() {
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
+        {/* SIGN IN */}
         <a
           href="#"
           className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
@@ -20,6 +23,41 @@ export default function Home() {
             memórias!
           </p>
         </a>
+
+        {/* HERO */}
+        <div className="space-y-5 ">
+          <Image src={nlwLogo} alt="NLWLOGO" />
+
+          <div className="max-w-[420px] space-y-1">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo.
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+          <a
+            href="#"
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+          >
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
+
+        {/* COPYRIGHT */}
+        <div className="text-sm leading-relaxed text-gray-200">
+          Feito com 💜 no NLW da{' '}
+          <a
+            // TARGET _BLANK PARA ABRIR O LINK EM OUTRA PÁGINA
+            target="_blank"
+            rel="noreferrer"
+            className="underline hover:text-gray-100"
+            href="https://rocketseat.com.br"
+          >
+            Rocketseat
+          </a>
+        </div>
       </div>
 
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
