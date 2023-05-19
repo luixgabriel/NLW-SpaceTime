301bc4eb213b5d841be5e8121ac3418f69e1916d
luix gabriel <luisgasbriel@gmail.com>
2023-05-18 22:18:59 -0300

    OAUTH GITHUB

---
+++ src/routes/auth.ts
@@ -0,0 +1,70 @@
+import { FastifyInstance } from 'fastify'
+import prisma from '../lib/prisma'
+import axios from 'axios'
+import { z } from 'zod'
+
+export async function authRoutes(app: FastifyInstance) {
+  app.post('/register', async (request) => {
+    console.log('cheguei aqui')
+    const bodySchema = z.object({
+      code: z.string(),
+    })
+
+    const { code } = bodySchema.parse(request.body)
+    console.log(code)
+
+    const accessTokenResponse = await axios.post(
+      'https://github.com/login/oauth/access_token',
+      null,
+      {
+        // CONFIGURAÇÕES DA REQUISIÇÃO, ESSES PARAMETROS SÃO AS QUERY STRINGS
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: 'application/json',
+        },
+      },
+    )
+
+    const { access_token } = accessTokenResponse.data
+
+    const userResponse = await axios.get('https://api.github.com/user', {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    })
+
+    const userSchema = z.object({
+      id: z.number(),
+      login: z.string(),
+      name: z.string(),
+      // .url() PRA VERIFICAR SE O DADO REALMENTE É UMA URL
+      avatar_url: z.string().url(),
+    })
+
+    const userInfo = userSchema.parse(userResponse.data)
+
+    let user = await prisma.user.findUnique({
+      where: {
+        githubId: userInfo.id,
+      },
+    })
+    if (!user) {
+      user = await prisma.user.create({
+        data: {
+          githubId: userInfo.id,
+          login: userInfo.login,
+          name: userInfo.login,
+          avatarUrl: userInfo.avatar_url,
+        },
+      })
+    }
+
+    return {
+      user,
+    }
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
@@ -8,6 +11,8 @@ const app = fastify()
 app.register(cors, {
   origin: true, // TODAS AS URLS DO FRONT PODERÃO ACESSAR NOSSO BACK
 })
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
