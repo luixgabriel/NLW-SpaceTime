5afa4103e32646be5db671c600ca94f1254fdd7a
luix gabriel <luisgasbriel@gmail.com>
2023-05-20 08:37:46 -0300

    saturday zzz

--- web/src/app/memories/new/page.tsx
+++ web/src/app/memories/new/page.tsx
@@ -25,11 +25,23 @@ const NewMemory = () => {
             htmlFor="isPublic"
             className="flex items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
           >
-            <input type="checkbox" name="isPublic" id="isPublic" value={true} />
+            <input
+              type="checkbox"
+              name="isPublic"
+              id="isPublic"
+              value={true}
+              className="h-4 w-4 rounded border-gray-400 bg-gray-700 text-purple-500"
+            />
             Tornar memória pública
           </label>
-          <input type="file" id="media" className="invisible" />
+          <input type="file" id="media" className="invisible h-0 w-0" />
         </div>
+        <textarea
+          name="content"
+          spellCheck={false}
+          className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
+          placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
+        />
       </form>
     </div>
   )
