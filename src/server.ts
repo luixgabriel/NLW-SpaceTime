1d0e69222b8878a84c171a629244382e01b7618e
luix gabriel <luisgasbriel@gmail.com>
2023-05-22 22:07:15 -0300

    feat: adding uploads routes and config

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,20 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadsRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 // APP REGISTER FUNCIONA COMO MIDDLEWARE PARECIDO COM O app.use()
 app.register(cors, {
@@ -19,10 +29,12 @@ app.register(jwt, {
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadsRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     // WINDOWS . ABRE O EMOJI PICKER
