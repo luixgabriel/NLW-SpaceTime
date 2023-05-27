3b1a0c4ffa750b8b0b45f54b3876514db9efdc0c
luix gabriel <luisgasbriel@gmail.com>
2023-05-27 10:25:18 -0300

    feat: selected memory by id

--- web/src/app/memories/[id]/page.tsx
+++ web/src/app/memories/[id]/page.tsx
@@ -1,19 +1,51 @@
-'use client'
 import { api } from '@/app/lib/api'
-import Cookie from 'js-cookie'
-import { useRouter } from 'next/navigation'
+import { cookies } from 'next/headers'
+import Image from 'next/image'
 
 interface Memory {
   id: string
   coverUrl: string
-  excerpt: string
+  content: string
+  isPublic?: boolean
   createdAt: string
+  userId: string
 }
 
-const SelectedMemory = async () => {
-  const router = useRouter()
-  const id = router.query.id
-  return <div>oi</div>
+interface IParams {
+  params: {
+    id: string
+  }
+}
+
+const SelectedMemory = async ({ params }: IParams) => {
+  const id = params.id
+  const isAuthenticated = cookies().has('token')
+  if (!isAuthenticated) {
+    return <h1>RALA DAQUI</h1>
+  }
+
+  const token = cookies().get('token')?.value
+  const response = await api.get(`memories/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  const selectedMemory: Memory = response.data
+  console.log(selectedMemory)
+
+  return (
+    <div>
+      <Image
+        src={selectedMemory.coverUrl}
+        width={592}
+        height={280}
+        alt="ai"
+        className="aspect-video w-full rounded-lg object-cover "
+      />
+      <p>{selectedMemory.content}</p>
+    </div>
+  )
 }
 
 export default SelectedMemory
